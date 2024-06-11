53a8105a041352adb60027bbf0b4a537bf178ca4
Abimael Brito <160194472+abimas59@users.noreply.github.com>
2024-06-10 21:54:35 -0300

    Santander BootCamp

---
+++ desafio-publicando/src/main/java/britt/desafio_publicando/DesafioPublicandoApplication.java
@@ -0,0 +1,13 @@
+package britt.desafio_publicando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPublicandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPublicandoApplication.class, args);
+	}
+
+}
---
+++ desafio-publicando/src/test/java/britt/desafio_publicando/DesafioPublicandoApplicationTests.java
@@ -0,0 +1,13 @@
+package britt.desafio_publicando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPublicandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
